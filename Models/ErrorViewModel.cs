dc64dbfb327f19813ea151a687e78bd617d59439
Julio3021 <julio_antaurco@usmp.pe>
2024-10-04 12:41:50 -0500

    primeros cambios

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace parcial_antaurco.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
